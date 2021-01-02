81e4edfb1d6509974e52931bff479da1a0b6f83a
Karuppiah Natarajan <karuppiah7890@gmail.com>
2021-01-02 21:21:15 +0530

    add code to get helm chart path as argument

--- main.go
+++ main.go
@@ -2,8 +2,18 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
 func main() {
-	fmt.Println("helm-unused-values plugin")
+	if len(os.Args) != 2 {
+		fmt.Println("pass exactly one argument")
+		fmt.Println("usage: helm-unused-values <helm-chart-path>")
+		os.Exit(1)
+		return
+	}
+
+	helmChartPath := os.Args[1]
+
+	fmt.Println(helmChartPath)
 }
